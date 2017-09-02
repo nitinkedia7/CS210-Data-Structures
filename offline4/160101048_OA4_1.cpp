172ef3d98a8a4888a045bfa5ad58ad574cae8768
Nitin Kedia <nitinkedia7@gmail.com>
2017-09-03 01:16:54 +0530

    Refactored and added comments to Q1,2 of OA4

--- offline4/160101048_OA4_1.cpp
+++ offline4/160101048_OA4_1.cpp
@@ -5,13 +5,13 @@
 #include <cstdlib>
 using namespace std;
 
-// quickSort sorts the integer array in ascending order with orden nlogn
+// swap interchanges (inplace) the elements in index i and j of a interger array
 void swap(int *array, int i, int j) {
 	int temp = array[i];
 	array[i] = array[j];
 	array[j] = temp;
 }
-
+// partition transfers elements less than pivot to left and others to right
 void partition(int *array, int l, int u, int *p) {
 	int pivot = array[*p];
 	swap(array, l, *p);
@@ -26,10 +26,10 @@ void partition(int *array, int l, int u, int *p) {
 	swap(array, down, u);
 	*p = u;
 }
-
+// quickSort sorts the integer array in ascending order with orden nlogn
 void quickSort(int *array, int l, int u) {
 	if (l < u) {
-		int p = (u+l)/2;
+		int p = (u+l)/2; // randomly selecting middle element as pivot
 		partition(array, l, u, &p);
 		quickSort(array, l, p-1);
 		quickSort(array, p+1, u);
@@ -50,11 +50,11 @@ int main() {
 		cin >> array[i];
 	}
 	for (int i = 0; i < n; i++) {
-		quickSort(array, 0, n-i-1); // sort given array decrementing its length progessively 
-		for (int j = 0; j < n-1-i ; j++) {
+		quickSort(array, 0, n-i-1); // sort given array decrementing its length by one in each iteration
+		for (int j = 0; j < n-1-i ; j++) { // taking difference of adjacent elements
 			array[j] = array[j+1]-array[j]; // since array is sorted ascendingly, array[j+1] >= array[j], thus difference will be automatically absolute
 		}
 	}
-	cout << array[0] << endl;
+	cout << array[0] << endl; // output
 	return 0;
 }
--- offline4/160101048_OA4_2.cpp
+++ offline4/160101048_OA4_2.cpp
@@ -5,23 +5,27 @@
 
 using namespace std;
 
-struct arrayNode {
+struct arrayNode { // structure for storing sparse matrix in array.
 	int row, column, value;
 };
-struct linkedNode {
+struct linkedNode { // structure for storing sparse matrix as linkedlist
 	int row, column, value;
-	linkedNode *down, *right;
+	linkedNode *down, *right; // for traversal along a column/row
 };
+
+// cityIndex assigns each city name a integer index as string manipulation is difficult
 int cityIndex(char **cities, int N, char *city) {
 	int i;
 	for (i=0; i<N; i++) {
-		if (strcmp(cities[i], "\0") == 0) break;
-		else if (strcmp(cities[i], city) == 0) return i;
+		if (strcmp(cities[i], city) == 0) return i; // returns index of the city if it is already present in the string array
+		else if (strcmp(cities[i], "\0") == 0) { 	// else add the city to the list and returns its index
+			strcpy(cities[i], city);
+			return i;
+		}
 	}
-	strcpy(cities[i], city);
-	return i;
 }
 
+// createArrayNode initialises a arrayNode with its row, column and value and returns its pointer
 arrayNode *createArrayNode(int row, int column, int value) {
 	arrayNode *head = new arrayNode;
 	head->row = row;
@@ -29,6 +33,7 @@ arrayNode *createArrayNode(int row, int column, int value) {
 	head->value = value;
 	return head;
 }
+// createLinkedNode also initialises a linkedNode with row, column and value and returns its pointer
 linkedNode *createLinkedNode(int row, int column, int value) {
 	linkedNode *head = new linkedNode();
 	head->row = row;
@@ -37,133 +42,105 @@ linkedNode *createLinkedNode(int row, int column, int value) {
 	return head;
 }
 
-void buildLinkedMatrix(linkedNode **linkedHeads, int F, int N, char **cities) {
-	for (int i=0; i<N; i++) {
-		linkedHeads[i] = createLinkedNode(-1, -1, -1);
-		linkedHeads[i]->down = linkedHeads[i];
-		linkedHeads[i]->right = linkedHeads[i];
+// buildArrayMatrix stores input distance matrix in array representation
+void buildArrayMatrix(arrayNode **arrayMatrix, int R, int N, char **cities) {
+	for (int i=0; i <= R; i++) {
+		if (i == 0) arrayMatrix[i] = createArrayNode(N, N, R); // first node contains number of rows(N), columns(N) and non-empty entries in the matrix
+		else {
+			char city1[25], city2[25]; int d;
+			scanf("%s %s %d", city1, city2, &d);
+			int city1Index = cityIndex(cities, N, city1); // find index of the city from cities array
+			int city2Index = cityIndex(cities, N, city2);
+			arrayMatrix[i] = createArrayNode(city1Index, city2Index, d);
+		}	
 	}
-	for (int i=0; i<N; i++) {
-		printf("%s ", cities[i]);
-	}printf("\n");
+}
 
+// buildLinkedMatrix stores input fare matrix in linked list representation
+void buildLinkedMatrix(linkedNode **linkedMatrix, int F, int N, char **cities) {
+	for (int i=0; i<N; i++) {
+		linkedMatrix[i] = createLinkedNode(-1, -1, -1); // assign arbitary row, column and values to each header element
+		linkedMatrix[i]->down = linkedMatrix[i]; 	// complete circular linkage
+		linkedMatrix[i]->right = linkedMatrix[i];
+	}
+	// scan F fare entries and insert them to the matrix
 	for (int i=0; i<F; i++) {
 		char city1[25], city2[25]; int f;
 		scanf("%s %s %d", city1, city2, &f);
-		int row = cityIndex(cities, N, city1);
+		int row = cityIndex(cities, N, city1); // find index of the city from cities array
 		int column = cityIndex(cities, N, city2);
-		cout << row << " " << column << " " << endl;
 
 		linkedNode *temp = createLinkedNode(row, column, f);
-		linkedNode *prev1, *next1;
-		prev1 = linkedHeads[column];
-		next1 = prev1->down;
-		while (next1 != linkedHeads[column] && next1->row < row) {
-			prev1 = next1;
-			next1 = next1->down;
+		
+		// adding new node to its respective column
+		linkedNode *prevCol, *nextCol;
+		prevCol = linkedMatrix[column];
+		nextCol = prevCol->down;
+		while (nextCol != linkedMatrix[column] && nextCol->row < row) { // navigate to required row in the column
+			prevCol = nextCol;
+			nextCol = nextCol->down;
 		}
-
-
-		//cout << i << endl;
-		linkedNode *prev, *next;
-		prev = linkedHeads[row];
-		next = prev->right;
-		while (next != linkedHeads[row] && next->column < column) {
-			prev = next;
-			next = next->right;
+		prevCol->down = temp; // link the new mode in the column chain
+		temp->down = nextCol;
+		
+		// adding new node to its respective row
+		linkedNode *prevRow, *nextRow;
+		prevRow = linkedMatrix[row];
+		nextRow = prevRow->right;
+		while (nextRow != linkedMatrix[row] && nextRow->column < column) { // navigate to required column in the row
+			prevRow = nextRow;
+			nextRow = nextRow->right;
 		}
-		prev1->down = temp;
-		temp->down = next1;
-		prev->right = temp;
-		temp->right = next;
-		cout << prev1->down->row << " " << prev1->down->column << endl;
-		cout << temp->down->row << " " << temp->down->column << endl;
-		cout << prev->right->row << " " << prev->right->column << endl;
-		cout << temp->right->row << " " << temp->right->column << endl;
-
+		prevRow->right = temp;
+		temp->right = nextRow ;
 	}
 }
-int search(linkedNode **linkedHeads, int row, int column) {
-	linkedNode *prev, *next;
-	prev = linkedHeads[row];
-	next = prev->right;
-	while (next != linkedHeads[row] && prev->column < column) {
-		prev = next;
-		next = next->right;
-	}
-	if (prev->column == column) {
-		return prev->value;
-	}
-	else return -1;
-}
-void print(linkedNode **linkedHeads, int N) {
-			linkedNode *prev, *next;
-	for (int i = 0; i<N; i++) {
 
-		prev = linkedHeads[i];
-		next = prev->right;
-		cout << prev->row << " " << prev->column << " "  << prev->value << endl;
-		while (next != linkedHeads[i]) {
-			cout << prev->row << " " << prev->column << " "  << prev->value << endl; 
-			prev = next;
-			next = next->right;
-		}
-	}
-	for (int i=0; i<N; i++) {
-		prev = linkedHeads[i];
-		next = prev->down;
-		cout << prev->row << " " << prev->column << " "  << prev->value << endl;
-		while (next != linkedHeads[i]) {
-			cout << prev->row << " " << prev->column << " "  << prev->value << endl; 
-			prev = next;
-			next = next->down;
-		}
-	}
+// searchFare returns the fare per km between two cities from linkedMatrix or -1 if corresponding entry is not found
+int searchFare(linkedNode **linkedMatrix, int city1, int city2) {
+	// assuming city1, city2 form a row, column pair
+	linkedNode *temp = linkedMatrix[city1];
+	do {
+		if (temp->column == city2) return temp->value;;
+		temp = temp->right;
+	}while (temp != linkedMatrix[city1]);
+	return -1;
 }
 
 int main() {
 	int N, R, F;
-	cin >> N >> R >> F;
+	cout << "Enter Input:" << endl;
+	cin >> N >> R >> F; // scan number of cities N, distance entries R, fare per km entries F
+	if (N <= 0) return 0;
 
-	char **cities = new char *[N];
+	char **cities = new char *[N]; // initialise a string array "cities" which assigns each city name an index [see cityIndex]
 	for (int i=0; i<N; i++) {
 		cities[i] = new char[25];
-		strcpy(cities[i], "\0");
+		strcpy(cities[i], "\0"); // initialise with null string
 	}
 
 	arrayNode *arrayMatrix[R+1];
-	arrayMatrix[0] = createArrayNode(N, N, R);
-	for (int i=1; i <= R; i = i+1) {
-		char city1[25], city2[25]; int d;
-		scanf("%s %s %d", city1, city2, &d);
-		int city1Index = cityIndex(cities, N, city1);
-		int city2Index = cityIndex(cities, N, city2);
-		arrayMatrix[i] = createArrayNode(city1Index, city2Index, d);
-	}
-	cout << "array matrix built" << endl;
-	for (int i=0; i<N; i++) {
-		printf("%s ", cities[i]);
-	}printf("\n");
+	buildArrayMatrix(arrayMatrix, R, N, cities);
+
 	linkedNode *linkedMatrix[N];
 	buildLinkedMatrix(linkedMatrix, F, N, cities);
-	//print(linkedMatrix, N);
 
-	int c1, c2, maxFare = 0;
+	int city1, city2, maxFare = -1;
+	// take distance between city1 and city2 from arrayMatrix and find corresponding fare per km from linkedMatrix
 	for (int i=1; i<=R; i++) {
-		// cout << maxFare << " " << endl;
-		cout << arrayMatrix[i]->row << " " << arrayMatrix[i]->column << " " << endl;
-		int farepkm = search(linkedMatrix, arrayMatrix[i]->row, arrayMatrix[i]->column);
-		if (farepkm == -1) {
-			farepkm = search(linkedMatrix, arrayMatrix[i]->column, arrayMatrix[i]->row);
-		}
+		int farepkm;
+		farepkm = searchFare(linkedMatrix, arrayMatrix[i]->row, arrayMatrix[i]->column);
+		if (farepkm == -1) farepkm = searchFare(linkedMatrix, arrayMatrix[i]->column, arrayMatrix[i]->row);
+		// eg- Delhi Dumbai in arrayMatrix and Mumbai Delhi in linkedMatrix
 		if (farepkm != -1) {
-			cout << farepkm << " " << endl;
-			if (farepkm*arrayMatrix[i]->value > maxFare) {
-				maxFare = farepkm*arrayMatrix[i]->value;
-				c1 = arrayMatrix[i]->row; c2 = arrayMatrix[i]->column;
-				cout << c1 << " " << c2 << " " << maxFare << endl;
+			int newMaxfare = farepkm * arrayMatrix[i]->value;
+			if (newMaxfare > maxFare) { // update maxFare
+				maxFare = newMaxfare;
+				city1 = arrayMatrix[i]->row;
+				city2 = arrayMatrix[i]->column;
 			}
 		}
 	}
-	cout << cities[c1] << " " << cities[c2] << " " << maxFare << endl;
+	if (maxFare > -1) cout << cities[city1] << " " << cities[city2] << " " << maxFare << endl;
+	else cout << "Insufficient Data" << endl;
 }
