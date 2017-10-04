7550cdf0d1839b6bfdad4142fa20e55059f3d31e
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-05 03:05:47 +0530

    Fully working binomial_heap Q4, OA6 & new input files

--- offline6/160101048_OA6_4.cpp
+++ offline6/160101048_OA6_4.cpp
@@ -12,94 +12,136 @@ struct node {
 
 node *create_node(int key) {
 	node *head = new node;
+	head->key = key;
 	head->degree = 0;
 	head->lchild = NULL;
 	head->sibling = NULL;
 	return head;
 }
 
+void show_heap_helper(node *head, int depth) {
+	if (head->sibling != NULL ) {
+		show_heap_helper(head->sibling, depth);
+	}
+	if (head->lchild != NULL) {
+		for (int i = 0; i <= depth; i++) cout << '\t';
+		cout << head->key;
+		if (head->lchild == NULL) cout << endl;
+	}
+	else {
+		cout << '\t';
+		cout << head->key;
+		cout << endl;
+	}
+	if (head->lchild != NULL) 
+		show_heap_helper(head->lchild, depth + 1);
+}
+void show_binomial_heap(node *head) {
+	cout << "Stucture of binomial heap rotated 90 degrees anticlockwise:" << endl;
+	if (head == NULL) cout << "Empty binomial heap." << endl;
+	else show_heap_helper(head, 0);
+}
+
 // makes x the parent of y
 void binomial_link(node *x, node *y) {
 	y->sibling = x->lchild;
 	x->lchild = y;
-	(x->degree)++;
+	x->degree += 1;
 }
 void binomial_merge(node *x, node *y, node **pHead) {
 	if (x != NULL && y != NULL) {
-		if (x->key < y->key) {
+		if (x->degree <= y->degree) {
 			*pHead = x;
-			binomial_merge(x->sibling, y, &(*pHead->sibling));
+			binomial_merge(x->sibling, y, &((*pHead)->sibling));
 		}
 		else {
 			*pHead = y;
-			binomial_merge(x, y->sibling, &(*pHead->sibling));
+			binomial_merge(x, y->sibling, &((*pHead)->sibling));
 		}
 	}
 	else {
 		if (y != NULL) {
 			*pHead = y;
-			binomial_merge(x, y->sibling, &(*pHead->sibling));
+			binomial_merge(x, y->sibling, &((*pHead)->sibling));
 		}
 		else if (x != NULL) {
 			*pHead = x;
-			binomial_merge(x->sibling, y, &(*pHead->sibling));			
+			binomial_merge(x->sibling, y, &((*pHead)->sibling));			
 		}
 		else *pHead = NULL;
 	}
 }
 node *binomial_heap_union(node *x, node *y) {
 	node *head = NULL;
-	head = binomial_merge(x, y, &head);
+	binomial_merge(x, y, &head);
 
 	node *node1 = head;
-	node *node0 = create_node(INT_MAX);
-	node0->sibling = node1;
+	node *node0 = NULL;
 	while (node1 != NULL) {
-		if (node1->sibling != NULL) {
-			node *node2 = node1->sibling;
+		node *node2 = node1->sibling;
+		if (node2 != NULL) {
+			node *node3 = node2->sibling;
 			if (node1->degree != node2->degree) {
 				node0 = node1; 
 				node1 = node2;
 			}
-			else {
-				node *node3 = node2->sibling;
+			else { 
 				if (node3 != NULL) {
 					if (node2->degree == node3->degree) {
 						node0 = node1;
 						node1 = node2; 
 					}
+					else {
+						if (node1->key < node2->key) {
+							binomial_link(node1, node2);
+							node1->sibling = node3;
+						}
+						else {
+							binomial_link(node2, node1);
+							if (node0 == NULL) head = node2;
+							else node0->sibling = node2;
+							node1 = node2;
+						}
+					}
 				}
 				else {
 					if (node1->key < node2->key) {
-						binomial_ink(node1, node2);
+						binomial_link(node1, node2);
 						node1->sibling = node3;
 					}
 					else {
 						binomial_link(node2, node1);
-						node0->sibling = node2;
+						if (node0 == NULL) head = node2;
+						else node0->sibling = node2;
 						node1 = node2;
 					}
 				}
 			}
 		}
+		else {
+			node0 = node1;
+			node1 = node2;
+		}
 	}
+	// show_binomial_heap(head);	
 	return head;
 }
 
 node *binomial_heap_insert(node *head1, int x) {
 	node *head2 = create_node(x);
 	node *head = binomial_heap_union(head1, head2);
+	// show_binomial_heap(head);
 	return head;
 }
 
 node *reverse(node *head) {
 	if (head != NULL) {
-		node *temp1 = head->ptr;
-		head->ptr = NULL;
+		node *temp1 = head->sibling;
+		head->sibling = NULL;
 		node *temp2;
 		while (temp1 != NULL) {
-			temp2 = temp1->ptr;
-			temp1->ptr = head;
+			temp2 = temp1->sibling;
+			temp1->sibling = head;
 			head = temp1;
 			temp1 = temp2;
 		}
@@ -111,19 +153,60 @@ int binomial_heap_extractMin(node **pHead) {
 	node *next = *pHead;
 
 	node *min = *pHead;
+	node *min_prev = NULL;
 	while (next != NULL) {
-		if (next->key < min->key) min = next;
+		if (next->key < min->key) {
+			min = next;
+			min_prev = prev;
+		}
 		prev = next;
 		next = next->sibling;
 	}
 
-	if (prev != NULL) prev->sibling = min->sibling;
+	if (min_prev != NULL) min_prev->sibling = min->sibling;
 	else *pHead = min->sibling;
 
 	int min_key = min->key;
-	node *head2 = min->lchild;
-	// free min
-	head2 = reverse(head2);
-	*pHead = binomial_heap_union(*pHead, head1)
+	node *head2 = reverse(min->lchild);
+	*pHead = binomial_heap_union(*pHead, head2);
+	return min_key;
 }
 
+int main() {
+	char cmd;
+	int arg;
+	char flag = '-';
+	node *head = NULL;
+	while (1) {
+		cin >> cmd;
+		if (cmd == '#') cin.ignore(10000, '\n');
+		else if (cmd == 'c') {
+			head = NULL;
+		}
+		else if (cmd == 'u') {}
+		else if (cmd == 'i') {
+			cin >> arg;
+			head = binomial_heap_insert(head, arg);
+		}
+		else if (cmd == 'd') {
+			if (head != NULL) {
+				arg = binomial_heap_extractMin(&head);
+				if (flag == '+') cout << "Minimum extracted: " << arg << endl;
+			}
+			else cout << "Cannot extractMin from empty heap." << endl;
+ 		}
+		else if (cmd == '+') {
+			flag = '+';
+			cout << "Print after extractMin: ON" << endl;
+		}
+		else if (cmd == '-') {
+			flag = '-';
+			cout << "Print after extractMin: OFF" << endl;
+
+		}
+		else if (cmd == 'S') {
+			show_binomial_heap(head);
+		}
+		else if (cmd == 'q') return 0;
+	}
+}
