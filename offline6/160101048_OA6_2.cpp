a96b02bdfe306b4a5078c2051f318a8d3265bb71
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-08 04:15:05 +0530

    added comments, removed redundant code

--- offline6/160101048_OA6_2.cpp
+++ offline6/160101048_OA6_2.cpp
@@ -5,46 +5,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// struct for storing individual customer in_time and cooking_time
 struct customer {
 	long long int o;
 	long long int c;
 };
-
+// findMin returns the customer who arrived earlier than the other
 int findMin(customer *array, int i, int j) {
 	if (array[i].c < array[j].c) return i;
 	else if (array[i].c  == array[j].c) {
 		if (array[i].o < array[j].o) return i;
-		else return j;
 	}
-	else return j;
+	return j;
 }
-
-bool isMin(customer first, customer second) {
-	if (first.c < second.c) return true;
-	else if (first.c == second.c) {
-		if (first.o < second.o) return true;
-	}
-	return false;
-}
-
-void swapper(customer *array, int i, int j) {
-	customer temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
-}
-
+/* Priority Queue subroutines start here */
 void minHeapify(customer *array, int n, int i) {
 	if (n > 2*i+2) {
 		int k = findMin(array, i, findMin(array, 2*i+1, 2*i+2));
 		if (k != i) {
-			swapper(array, i, k);
+			swap(array[i], array[k]);
 			minHeapify(array, n, k);
 		}
 	}
 	else if (n == 2*i+2) {
 		int k = findMin(array, i , 2*i+1);
 		if (k != i) {
-			swapper(array, i, k);
+			swap(array[i], array[k]);
 			minHeapify(array, n, k);
 		} 
 	}  	
@@ -61,26 +47,24 @@ void insert(customer *array, int *pn, customer x) {
 	array[i] = x;
 	int p = (i-1)/2;
 	while (p >= 0) {
-		if (isMin(array[i], array[p]))  {
-			swapper(array, i, p);
+		if (i != p && i == findMin(array, i, p))  {
+			swap(array[i], array[p]);
 			i = p;
 			p = (i-1)/2;
-
 		}
-		else {
-			break; 
-		}
+		else break;
 	}
-	*pn = *pn+1;
+	(*pn)++;
 }
 
 customer extractMin(customer *array, int *pn) {
 	customer min = array[0];
 	array[0] = array[*pn-1];
-	(*pn)--;
-	minHeapify(array, *pn, 0);
+	
+	minHeapify(array, --(*pn), 0);
 	return min;
 }
+/* Priority Queue subroutines ends here */
 
 void print(customer *array, int n) {
 	for (int i = 0; i < n; i++) {
@@ -88,21 +72,22 @@ void print(customer *array, int n) {
 	}
 	cout << endl;
 }
+/* Quicksort subroutines start here */
 void partition(customer *array, int l, int u, int *p) {
 	customer pivot = array[*p];
-	swapper(array, l, *p);
+	swap(array[l], array[*p]);
 	int down = l;
 	while (l < u) {
 		while (array[l].o <= pivot.o && l < u) l++;
 		while (array[u].o > pivot.o) u--;
 		if (l < u) {
-			swapper(array, l, u);
+			swap(array[l], array[u]);
 		}
 	}
-	swapper(array, down, u);
+	swap(array[down], array[u]);
 	*p = u;
 }
-// quickSort sorts the integer array in ascending order with orden nlogn
+// quickSort sorts customers by in_time
 void quickSort(customer *array, int l, int u) {
 	if (l < u) {
 		int p = (u+l)/2; // randomly selecting middle element as pivot
@@ -111,6 +96,7 @@ void quickSort(customer *array, int l, int u) {
 		quickSort(array, p+1, u);
 	}
 }
+/* Quicksort subroutines end here */
 
 int main() {
 	int n;
@@ -118,36 +104,44 @@ int main() {
 	cin >> n;
 	if (n <= 0) return 0;
 
-	customer in_array[n];
+	customer in_array[n]; // scanning incoming customers
 	for (int i = 0; i < n; i++) {
 		cin >> in_array[i].o >> in_array[i].c;
 	}
-	quickSort(in_array, 0, n-1);
+	quickSort(in_array, 0, n-1); // sorting customers by in_time
+
+	int i = 0, j = i+1;
+	long long int current_time = 0, wait_time = 0;
 
 	customer serve_array[n];
-	int i = 0, j = i+1;
-	long long int time_elapsed = 0, total_wait_time = 0;
-	serve_array[i] = in_array[i]; 
-	customer priority_queue[n]; int l = 0;
+	serve_array[i] = in_array[i]; // serve_array maintains customers by the order in which they were served
+
+	customer priority_queue[n]; int l = 0; // initialising an empty priority queue
+
+	// order i'th customer in serve_array is being cooked,
+	// we insert all customers who arrive before this order will be cooked in a priority queue
+	// now the extractMin customer is the choice for minimising total average waiting time
+	// repeat, but if a customer arrives at a time when no cake is being cooked, the cycle essentially starts again;  
 	do {
-		if (time_elapsed > serve_array[i].o) time_elapsed += serve_array[i].c;
-		else time_elapsed = serve_array[i].o + serve_array[i].c;
+		if (current_time > serve_array[i].o) current_time += serve_array[i].c;
+		else current_time = serve_array[i].o + serve_array[i].c;
 
-		total_wait_time += (time_elapsed - serve_array[i].o);
+		wait_time += (current_time - serve_array[i].o);
 
-		while (j < n && in_array[j].o <= time_elapsed) {
-			insert(priority_queue, &l, in_array[j]);
-			j++;
+		while (j < n && in_array[j].o <= current_time) {
+			insert(priority_queue, &l, in_array[j++]);
 		}		
+		
 		i++;
 		if (l > 0) serve_array[i] = extractMin(priority_queue, &l);
-		if (j <= i) {
+		else {
 			serve_array[i] = in_array[i];
 			j = i+1;
 		}
 	} while (i < n);
-	//print(serve_array, n);
+
 	cout << "Minimum average waiting time:" << endl;
-	cout << (total_wait_time/n) << endl;
+	long long int avg_wait_time = (wait_time/n);
+	cout << avg_wait_time << endl;
 	return 0;
 }
--- offline6/160101048_OA6_3.cpp
+++ offline6/160101048_OA6_3.cpp
@@ -4,18 +4,18 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_SIZE = 1000;
-
+const int MAX_SIZE = 1000; // for backend minHeap
+// struct for storing command and its argument, for removeMin argument is arbitary
 struct node {
 	string cmd;
 	int arg;
 };
-
+// finMin returns the index with lower value
 int findMin(int *array, int i, int j) {
 	if (array[i] < array[j]) return i;
 	else return j;
 }
-
+// Subroutines for a minHeap
 void min_heapify(int *min_heap, int i, int n) {
 	if (n >= 2*i+2) {
 		int k = -1;
@@ -31,14 +31,14 @@ void min_heapify(int *min_heap, int i, int n) {
 		}
 	}
 }
-
+// Returns and deletes the minimum element from a minHeap
 int remove_min(int *min_heap, int *pn) {
 	int min = min_heap[0];
 	min_heap[0] = min_heap[(*pn)-1];
 	min_heapify(min_heap, 0, --(*pn));
 	return min;
 }
-
+// inserts a new element in a minHeap
 void insert(int *min_heap, int *pn, int arg) {
 	int i = (*pn)++;
 	min_heap[i] = arg;
@@ -49,7 +49,7 @@ void insert(int *min_heap, int *pn, int arg) {
 		p = (i-1)/2;
 	}
 }
-
+// Utility function for printing command sequence
 void print_seq(node *seq, int n) {
 	for (int i = 0; i < n; i++) {
 		cout << seq[i].cmd << " ";
@@ -58,43 +58,44 @@ void print_seq(node *seq, int n) {
 	}
 }
 
+// find_output_sequence makes the corrected command sequence from input sequence recursively
 void find_output_seq(node *i_seq, int i, int n, int *min_heap, int *pl, node *o_seq, int *po) {
 	if (i < n) {
-		if (i_seq[i].cmd == "insert")  {
+		if (i_seq[i].cmd == "insert")  { // if input is insert output must also be insert
 			insert(min_heap, pl, i_seq[i].arg);
 			o_seq[(*po)].cmd = "insert";
 			o_seq[(*po)++].arg = i_seq[i].arg;
 		}
-		else if (i_seq[i].cmd == "getMin") {
+		else if (i_seq[i].cmd == "getMin") { // if input is getMin compare its argument with getMin from backend minHeap
 			while ((*pl) >= 0) {
-				if ((*pl) == 0) {
+				if ((*pl) == 0) { // if minHeap is empty, insert argument of input getMin in minHeap, hence output is insert
 					insert(min_heap, pl, i_seq[i].arg);
 					o_seq[(*po)].cmd = "insert";
 					o_seq[(*po)++].arg = i_seq[i].arg;
 				}
-				if (i_seq[i].arg == min_heap[0]) {
+				if (i_seq[i].arg == min_heap[0]) { // if getMin arg matches with current getMin then output will also be getMin
 					o_seq[(*po)].cmd = "getMin";
 					o_seq[(*po)++].arg = i_seq[i].arg;
 					break;
 				}
 				else {
-					int min = remove_min(min_heap, pl);
+					int min = remove_min(min_heap, pl); // if input getMin doesn't match then removeMin from minHeap till we arrive at above cases
 					o_seq[(*po)].cmd = "removeMin";
 					o_seq[(*po)++].arg = min;
 				}
 			}
 		}
-		else if (i_seq[i].cmd == "removeMin") {
+		else if (i_seq[i].cmd == "removeMin") { 
 			if ((*pl) == 0) {
-				insert(min_heap, pl, i_seq[i].arg);
+				insert(min_heap, pl, i_seq[i].arg); // if input is removeMin when minHeap is empty then insert 0 in minHeap
 				o_seq[(*po)].cmd = "insert";
 				o_seq[(*po)++].arg = i_seq[i].arg;
 			}
 			int min = remove_min(min_heap, pl);
-			o_seq[(*po)].cmd = "removeMin";
+			o_seq[(*po)].cmd = "removeMin"; // else output is also removeMin
 			o_seq[(*po)++].arg = min;
 		}
-		find_output_seq(i_seq, i+1, n, min_heap, pl, o_seq, po);
+		find_output_seq(i_seq, i+1, n, min_heap, pl, o_seq, po); // proceed to correct next input command
 	}
 }
 
@@ -103,7 +104,7 @@ int main() {
 	cout << "Enter input sequence: " << endl;
 	cin >> n;
 
-	node i_seq[n];
+	node i_seq[n]; // sccanning input commangd & arguments into a array for processing
 	for (int i = 0; i < n; i++) {
 		cin >> i_seq[i].cmd;
 		if (i_seq[i].cmd == "removeMin") {
@@ -112,13 +113,13 @@ int main() {
 		else cin >> i_seq[i].arg; 
 	}
 	// print_seq(i_seq, n);
-	node o_seq[MAX_SIZE];
+	node o_seq[MAX_SIZE]; // initialising output sequence,
 	int o = 0;
 
-	int min_heap[MAX_SIZE];
+	int min_heap[MAX_SIZE]; // maintaining a minHeap in backend
 	int l = 0;
 	
-	find_output_seq(i_seq, 0, n, min_heap, &l, o_seq, &o);
+	find_output_seq(i_seq, 0, n, min_heap, &l, o_seq, &o); // generating output sequence
 	
 	cout << endl << "Corrected sequence: " << endl;
 	cout << o << endl;
