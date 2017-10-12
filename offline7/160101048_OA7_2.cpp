989f40a589de116fc0f1beb29290d3bc9dbb7781
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-13 03:19:11 +0530

    Working fib heap, verified by random inputs

--- offline7/160101048_OA7_2.cpp
+++ offline7/160101048_OA7_2.cpp
@@ -8,7 +8,6 @@ using namespace std;
 struct node {
 	int degree, key;
 	node *parent, *child, *next, *prev;
-	// bool mark;
 };
 
 node *create_node(int key) {
@@ -18,27 +17,24 @@ node *create_node(int key) {
 	head->child = NULL;
 	head->next = head; head->prev = head;
 }
-
 void show_heap_helper(node *head, int depth) {
-	if (head->next != head) { 
-		show_heap_helper(head->next, depth);
+	if (head != NULL) {
+		node *temp = head;
+		do {
+			for (int i = 0; i <= depth; i++) cout << '\t';
+			cout << '{' << temp->key << ',' << temp->degree << '}' << endl;
+			if (temp->child != NULL) {
+				show_heap_helper(temp->child, depth+1);
+			}
+			temp = temp->prev;
+		} while (temp != head && temp != NULL);
 	}
-	if (head->child != NULL) {
-		for (int i = 0; i <= depth; i++) cout << '\t';
-		cout << head->key;
-		if (head->child == NULL) cout << endl;
-	}
-	else {
-		cout << '\t';
-		cout << head->key;
-		cout << endl;
-	}
-	if (head->child != NULL) 
-		show_heap_helper(head->child, depth + 1);
+	return;
 }
+
 void show_fib_heap(node *head) {
 	cout << "Stucture of fibonacci heap rotated 90 degrees anticlockwise:" << endl;
-	if (head == NULL) cout << "Empty fibonnaci heap." << endl;
+	if (head == NULL) cout << "Empty fibonacci heap." << endl;
 	else show_heap_helper(head, 0);
 }
 
@@ -51,11 +47,11 @@ node *fib_heap_union(node *head1, node *head2) {
 		node *head1_next = head1->next;
 		node *head2_next = head2->next;
 
-		head1->next = head2->next;
-		head2->next->prev = head1;
+		head1->next = head2_next;
+		head2_next->prev = head1;
 
 		head2->next = head1_next;
-		head1->next->prev = head2;
+		head1_next->prev = head2;
 		if (head1->key < head2->key) head = head1;
 		else head = head2;
 	}
@@ -70,32 +66,41 @@ void fib_heap_insert(node **pHead, int *pn, int key) {
 }
 // make x the parent of y
 node *fib_heap_link(node *y, node *x) {
-	y->prev->next = y->next;
 	y->next->prev = y->prev;
+	y->prev->next = y->next;
 
+	y->next = y; y->prev = y;
 	x->child = fib_heap_union(y, x->child);
+	y->parent = x;
 	x->degree += 1;
 	return x;	
 }
 
 node *fib_heap_consolidate(node *head, int n) {
-	int k = log2(n);
-	node *array[k] = {NULL};
+	int k = floor(log2(n));
+	node *array[k+1] = {NULL};
 
 	node *temp = head;
 	do {
 		node *x = temp;
+		temp = temp->next;
 		int d = x->degree;
 		while (array[d] != NULL) {
 			node *y = array[d];
-			if (x->key < y->key) x = fib_heap_link(y, x);
-			else x = fib_heap_link(x, y);
+			if (x->key > y->key) {
+				node *swap = x;
+				x = y;
+				y = swap;
+			}
+			if (head == y) head = head->next;
+			if (temp == y) temp = temp->next;
+ 			x = fib_heap_link(y, x);
 			array[d++] = NULL;
 		}
 		array[d] = x;
 	} while (temp != head);
 	node *new_head = NULL;
-	for (int i = 0; i < k; i++) {
+	for (int i = 0; i <= k; i++) {
 		if (array[i] != NULL) {
 			array[i]->next = array[i];
 			array[i]->prev = array[i];
@@ -116,6 +121,7 @@ int fib_heap_extract_min(node **pHead, int *pn) {
 			do {
 				temp->parent = NULL;
 				if (temp->key < child->key) child = temp;
+				temp = temp->next;
 			} while (temp != head->child);
 
 		}
@@ -128,9 +134,21 @@ int fib_heap_extract_min(node **pHead, int *pn) {
 			(*pn)--;
 		}
 		*pHead = fib_heap_union(head, child);
-
-		*pHead = fib_heap_consolidate(*pHead, *pn);
+		if (*pHead != NULL)
+			*pHead = fib_heap_consolidate(*pHead, *pn);
 		return min_key;
+	}
+}
+void free_fib_heap(node *head, node *stop) {
+	if (head != NULL) {
+		if (head->next != stop ) {
+			free_fib_heap(head->next, stop);
+		}
+		if (head->child != NULL) {
+			free_fib_heap(head->child, head->child);
+		}
+		// cout << "Deleted " << head->key << endl;
+		delete head;		
 	}
 }
 
@@ -143,7 +161,7 @@ int main() {
 	while (cin >> cmd) {
 		if (cmd == '#') cin.ignore(10000, '\n'); // ignore comment line
 		else if (cmd == 'c') { 
-			// free_binomial_heap(head);
+			free_fib_heap(head, head);
 			head = NULL;
 		}
 		else if (cmd == 'i') {
---
+++ offline7/random_fib_seq.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	
+	cout << 'c' << endl;
+	cout << '+' << endl;
+	
+	srand(time(NULL));
+	int arg;
+	for (int i = 0; i < n; i++) {
+		arg = rand() % 100*n + 1;
+		cout << "i " << arg << endl;
+	}
+	for (int i = 0; i < n+2; i++) {
+		if (i%(n/10) == 0) cout << 'S' << endl;
+		cout << 'e' << endl; 
+	}
+}
