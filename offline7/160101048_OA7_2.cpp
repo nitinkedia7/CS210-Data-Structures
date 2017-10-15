2ca1d311a999f434ed38991e08821b92b02b3cb4
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-15 13:09:29 +0530

    incl only relevant lib, tweaked consolidate calls, comments Q2

--- offline7/160101048_OA7_2.cpp
+++ offline7/160101048_OA7_2.cpp
@@ -1,49 +1,51 @@
 // 160101048_OA7_2.cpp: Nitin Kedia
-// Description: Implementation of Fibonacci Heap
+// Description: Implementation of Fibonacci Heap with CLI
 
 #include <iostream>
-#include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
-struct node {
+struct node { // struct for each node of fibonacci heap
 	int degree, key;
 	node *parent, *child, *next, *prev;
 };
 
-node *create_node(int key) {
+node *create_node(int key) { // initialise a pointer to node with given key
 	node *head = new node;
 	head->key = key; head->degree = 0;
 	head->parent = NULL;
 	head->child = NULL;
 	head->next = head; head->prev = head;
+	return head;
 }
-void show_heap_helper(node *head, int depth) {
+
+void show_heap_helper(node *head, int depth) { // show_heap_helper recursively prints the fibonacci heap in proper format
 	if (head != NULL) {
 		node *temp = head;
 		do {
 			for (int i = 0; i <= depth; i++) cout << '\t';
-			cout << '{' << temp->key << ',' << temp->degree << '}' << endl;
-			if (temp->child != NULL) {
+			cout << '{' << temp->key << ',' << temp->degree << '}'<< endl;
+			if (temp->child != NULL) { // traverse child(s)
 				show_heap_helper(temp->child, depth+1);
 			}
 			temp = temp->prev;
-		} while (temp != head && temp != NULL);
+		} while (temp != head && temp != NULL); // traverse siblings
 	}
 	return;
 }
 
-void show_fib_heap(node *head) {
+void show_fib_heap(node *head) { // show_fib_heap complements and drives its helper function
 	cout << "Stucture of fibonacci heap rotated 90 degrees anticlockwise:" << endl;
 	if (head == NULL) cout << "Empty fibonacci heap." << endl;
 	else show_heap_helper(head, 0);
 }
 
-node *fib_heap_union(node *head1, node *head2) {
+node *fib_heap_union(node *head1, node *head2) { // concats two doubly linked lists and also adjusts the min pointer
 	node *head = NULL;
 	// concat root list of head1 with head2
-	if (head1 == NULL) head = head2;
+	if (head1 == NULL) head = head2; // one of the lists is NULL
 	else if (head2 == NULL) head = head1;
-	else {
+	else {							 // both lists are not NULL
 		node *head1_next = head1->next;
 		node *head2_next = head2->next;
 
@@ -52,37 +54,37 @@ node *fib_heap_union(node *head1, node *head2) {
 
 		head2->next = head1_next;
 		head1_next->prev = head2;
-		if (head1->key < head2->key) head = head1;
+		if (head1->key < head2->key) head = head1; // head must point to min
 		else head = head2;
 	}
 	return head;
 }
 
-void fib_heap_insert(node **pHead, int *pn, int key) {
+void fib_heap_insert(node **pHead, int *pn, int key) { // insert a new key into fibonacci heap 
 	node *head1 = *pHead;
-	node *head2 = create_node(key);
+	node *head2 = create_node(key); // create a fib_heap with single node key and merge it to main heap
 	(*pn)++;
 	*pHead = fib_heap_union(head1, head2);
 }
-// make x the parent of y
-node *fib_heap_link(node *y, node *x) {
-	y->next->prev = y->prev;
+
+node *fib_heap_link(node *y, node *x) { // make x the parent of y
+	y->next->prev = y->prev; // remove y from its doubly linked list
 	y->prev->next = y->next;
 
-	y->next = y; y->prev = y;
-	x->child = fib_heap_union(y, x->child);
+	y->next = y; y->prev = y; // isolate y
+	x->child = fib_heap_union(y, x->child); // merge y into child list of x
 	y->parent = x;
 	x->degree += 1;
 	return x;	
 }
 
-node *fib_heap_consolidate(node *head, int n) {
+node *fib_heap_consolidate(node *head, int n) { // transform root list such that no two nodes in it have the same degree 
 	int k = floor(log2(n));
-	node *array[k+1] = {NULL};
+	node *array[k+1] = {NULL}; // auxillary array
 
-	node *temp = head;
+	node *temp = head; // iterate over the root list
 	do {
-		node *x = temp;
+		node *x = temp; // merge x with other elements of same degree till it becomes a node of unique degree
 		temp = temp->next;
 		int d = x->degree;
 		while (array[d] != NULL) {
@@ -92,25 +94,26 @@ node *fib_heap_consolidate(node *head, int n) {
 				x = y;
 				y = swap;
 			}
-			if (head == y) head = head->next;
-			if (temp == y) temp = temp->next;
+			if (head == y) head = head->next; // consolidate distorts the root list, but head must always be in root list
+			if (temp == y) temp = temp->next; // same for y
  			x = fib_heap_link(y, x);
 			array[d++] = NULL;
 		}
 		array[d] = x;
 	} while (temp != head);
-	node *new_head = NULL;
+
+	node *new_head = NULL; // make a new root list from elements of auxillary array
 	for (int i = 0; i <= k; i++) {
 		if (array[i] != NULL) {
-			array[i]->next = array[i];
+			array[i]->next = array[i]; // isolate array[i]
 			array[i]->prev = array[i];
-			new_head = fib_heap_union(new_head, array[i]);
+			new_head = fib_heap_union(new_head, array[i]); // insert it into doubly linked list
 		}
 	}
 	return new_head;
 }
 
-int fib_heap_extract_min(node **pHead, int *pn) {
+int fib_heap_extract_min(node **pHead, int *pn) { // return and delete the minimum key from fibonacci heap
 	node *head = *pHead;
 	if (head != NULL) {
 		int min_key = head->key;
@@ -119,27 +122,29 @@ int fib_heap_extract_min(node **pHead, int *pn) {
 		node *child = temp;
 		if (temp != NULL) {
 			do {
-				temp->parent = NULL;
+				temp->parent = NULL; // make parent = NULL for all child of head
 				if (temp->key < child->key) child = temp;
 				temp = temp->next;
 			} while (temp != head->child);
 
 		}
 
-		if (head == head->next) head = NULL;
-		else {
-			head->prev->next = head->next;
-			head->next->prev = head->prev;
-			head = head->next;
-			(*pn)--;
+		if (head == head->next) { // if root has single element head then new head is just its child list
+			*pHead = child;
 		}
-		*pHead = fib_heap_union(head, child);
-		if (*pHead != NULL)
+		else {
+			head->prev->next = head->next; // remove head from root list
+			head->next->prev = head->prev;
+
+			*pHead = fib_heap_union(head->next, child); // concat new root list and child list of head and consolidate
 			*pHead = fib_heap_consolidate(*pHead, *pn);
+		}
+		delete head; // free head
+		(*pn)--;     // reduce heap size
 		return min_key;
 	}
 }
-void free_fib_heap(node *head, node *stop) {
+void free_fib_heap(node *head, node *stop) { // recusively free all nodes of fib heap user defines new heap
 	if (head != NULL) {
 		if (head->next != stop ) {
 			free_fib_heap(head->next, stop);
@@ -152,6 +157,11 @@ void free_fib_heap(node *head, node *stop) {
 	}
 }
 
+node *make_fib_heap(node *head) {
+	free_fib_heap(head, head);
+	return NULL;
+}
+
 int main() {
 	char cmd; // command
 	int arg;  // argument (if any)
@@ -160,23 +170,22 @@ int main() {
 	int n = 0;
 	while (cin >> cmd) {
 		if (cmd == '#') cin.ignore(10000, '\n'); // ignore comment line
-		else if (cmd == 'c') { 
-			free_fib_heap(head, head);
-			head = NULL;
+		else if (cmd == 'c') {  // MAKE-FIB-HEAP
+			head = make_fib_heap(head);
 		}
-		else if (cmd == 'i') {
+		else if (cmd == 'i') { // FIB-HEAP-INSERT
 			cin >> arg;
 			fib_heap_insert(&head, &n, arg);
 		}
-		else if (cmd == 'u') {
+		else if (cmd == 'u') { // FIB-HEAP-UNION
 			cout << "Only used internally like in insert, extractMin." << endl;
 		} 
-		else if (cmd == 'e') {
+		else if (cmd == 'e') { // FIB-HEAP-EXTRACT-MIN
 			if (head != NULL) {
 				arg = fib_heap_extract_min(&head, &n);
 				if (flag == '+') cout << "Minimum extracted: " << arg << endl;
 			}
-			else cout << "Cannot extractMin from empty heap." << endl;
+			else cout << "Cannot extract-min from empty heap." << endl;
  		}
 		else if (cmd == '+') {
 			flag = '+';
@@ -187,9 +196,10 @@ int main() {
 			cout << "Print after extractMin: OFF" << endl;
 
 		}
-		else if (cmd == 'S') {
+		else if (cmd == 'S') { // SHOW-FIB-HEAP
 			show_fib_heap(head);
 		}
 		else if (cmd == 'q') return 0; // q for quit
 	}
+	return 0;
 }
