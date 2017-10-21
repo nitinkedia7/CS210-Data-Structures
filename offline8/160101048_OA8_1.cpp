5425f138e6af2b644f5275f379023e940a3144d7
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-21 19:59:15 +0530

    Solved both components (int component) and cycles(recursion stack)

--- offline8/160101048_OA8_1.cpp
+++ offline8/160101048_OA8_1.cpp
@@ -11,7 +11,7 @@ struct adj_node {
 
 struct node {
 	int color, parent;
-	int d, f, printed;
+	int component;
 	adj_node *link;
 };
 
@@ -23,57 +23,87 @@ void enqueue(adj_node **pHead, int index) {
 	*pHead = temp;
 }
 
-void dfs_visit(node *array, int n, int i, int *ptime) {
+void print_path(node *array, int n, int i, int j) {
+	if (i == j) {
+		cout << j << " ";
+		return;
+	}
+	print_path(array, n, i, array[j].parent);
+	cout << j << " ";
+}
+
+void dfs_visit(node *array, int n, int i, adj_node **cycle_head) {
 	array[i].color = 0;
-	array[i].d = ++(*ptime);
 	adj_node *head = array[i].link;
 	while (head != NULL) {
 		int k = head->index;
 		if (array[k].color == -1) {
 			array[k].parent = i;
-			dfs_visit(array, n, k, ptime);
+			array[k].component = array[i].component;
+			dfs_visit(array, n, k, cycle_head);
+		}
+		else if (array[i].parent != k) {
+			if (array[k].color == 0) {
+				enqueue(cycle_head, i);
+				enqueue(cycle_head, k);
+			}
 		}
 		head = head->ptr;
 	}
-	array[i].f = ++(*ptime);
 	array[i].color = 1;
 }
 
-void dfs(node *array, int n) {
-	int time = 0;
+void dfs(node *array, int n, int *pcomponent, adj_node **cycle_head) {
 	for (int i = 0; i <= n; i++) {
 		if (array[i].color == -1) {
-			dfs_visit(array, n, i, &time);
+			array[i].component = ++(*pcomponent);
+			dfs_visit(array, n, i, cycle_head);
 		}
 	}
 }
 
-void print_connected(node *array, int n) {
-	dfs(array, n);
+void print_connected(node *array, int n, int component) {
+	adj_node **ans = (adj_node**)calloc(component+1, sizeof(adj_node*));
+	
 	for (int i = 1; i <= n; i++) {
-		if (array[i].printed == 0) {
-			for (int j = 1; j <= n; j++) {
-				if (array[j].d >= array[i].d && array[j].f <= array[i].f) {
-					array[j].printed = 1;
-					cout << j << " ";
-				}
-			}
-			cout << endl;
-		}
+		enqueue(&(ans[array[i].component]), i);
 	}
-}
-
-void print(node *array, int n) {
-	for (int i = 1; i <= n; i++) {
-		cout << i << " " << array[i].d << " " << array[i].f << endl;
-		adj_node *head = array[i].link;
+	for (int i = 1; i <= component; i++) {
+		adj_node *head = ans[i];
+		cout << "component" << i << ": ";
 		while (head != NULL) {
 			cout << head->index << " ";
 			head = head->ptr;
 		}
-		cout << "NULL" << endl << endl;
+		cout << endl;
 	}
 }
+
+void print_cycles(node *array, int n, adj_node *head) {
+	if (head == NULL) {
+		cout << "No cycle exists." << endl;
+		return;
+	} 
+	int i = 0;
+	while (head != NULL) {
+		cout << "cycle" << ++i << ": "; 
+		print_path(array, n, head->index, head->ptr->index);
+		cout << head->index << endl;
+		head = head->ptr->ptr;
+	}
+}
+
+// void print(node *array, int n) {
+// 	for (int i = 1; i <= n; i++) {
+// 		cout << i << " " << array[i].parent << endl;
+// 		adj_node *head = array[i].link;
+// 		while (head != NULL) {
+// 			cout << head->index << " ";
+// 			head = head->ptr;
+// 		}
+// 		cout << "NULL" << endl << endl;
+// 	}
+// }
 
 int main() {
 	int n, e;
@@ -83,19 +113,22 @@ int main() {
 	node array[n+1];
 	for (int i = 1; i <= n; i++) {
 		array[i].color = -1;
-		array[i].d = -1;
-		array[i].f = -1;
 		array[i].parent = -1;
 		array[i].link = NULL;
-		array[i].printed = 0;
+		array[i].component = 0;
 	}
 	int e1, e2;
 	for (int i = 0; i < e; i++) {
-		scanf("%d, %d", &e1, &e2);
+		cin >> e1 >> e2;
+		// scanf("%d, %d", &e1, &e2);
 		enqueue(&(array[e1].link), e2);
 		enqueue(&(array[e2].link), e1);
 	}
+		
+	int component = 0;
+	adj_node *cycle_head = NULL;
+	dfs(array, n, &component, &cycle_head);
 	// print(array, n);
-	print_connected(array, n);
-	// print(array, n);
+	print_connected(array, n, component);
+	print_cycles(array, n, cycle_head);
 } 
