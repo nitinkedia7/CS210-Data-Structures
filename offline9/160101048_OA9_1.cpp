0d9b047b9842f1fbdf3dc1be6f261ed9966cf13d
Nitin Kedia <nitinkedia7@gmail.com>
2017-10-29 17:21:30 +0530

    merge max_subtree and output fn to dfs_visit, add comment

--- offline9/160101048_OA9_1.cpp
+++ offline9/160101048_OA9_1.cpp
@@ -1,21 +1,22 @@
 // 160101048_OA9_1.cpp: Nitin Kedia
-// Description: Tower of cubes
+// Description: Tower of cubes using DFS
+
+// Ordered pair (cube, face) denote a distinct node. "f" is the maximum height of the tower that can be formed if (cube, face) is topmost
 
 #include <iostream>
 using namespace std;
 
-struct adj_cube {
+struct adj_cube { // struct for adjacent nodes in a linked list
 	int index, face;
 	adj_cube *ptr;
 };
 
-struct cube {
-	int visit_color[6], d[6], f[6], parent[6][2];
-	int face_colors[6];
+struct cube { // struct for each cube
+	int visit_face[6], f[6], face_color[6]; // a cube can be uniquely visited through six faces, so all properties are in array[6]
 	adj_cube *link[6];
 };
 
-void prepend(adj_cube **pHead, int index, int face) {
+void insert(adj_cube **pHead, int index, int face) { // insert into front of a link list
 	adj_cube *temp = new adj_cube;
 	temp->index = index;
 	temp->face = face;
@@ -24,86 +25,51 @@ void prepend(adj_cube **pHead, int index, int face) {
 	*pHead = temp;
 }
 
-int opposite_face(int face) {
+int opposite_face(int face) { // return opposite_face, [0:front, 1:back, 2:left, 3:right, 4:top, 5:bottom]
 	if (face % 2 == 0) return face+1;
 	else return face-1;
 }
-void max_subtree(cube *array, int n, int ui, int uf) {
-	int opp_face = opposite_face(uf);
-	adj_cube *head = array[ui].link[opp_face];
 
-	int h = 0;
-	while (head != NULL) {
-		int vi = head->index;
-		int vf = head->face;
-		if (array[vi].visit_color[vf] == 1) {
-			if (array[vi].f[vf] > h) {
-				h = array[vi].f[vf];
-			}
-		}
-		head = head->ptr;
-	}
-	array[ui].f[uf] = h+1;	
-}
+void dfs_visit(cube *array, int n, int index, int face, int *mi, int *mf) {
+	array[index].visit_face[face] = 0; // grey
 
-
-
-void dfs_visit(cube *array, int n, int index, int face) {
-	array[index].visit_color[face] = 0;
-	// if (array[index].d[face] < *d)
-	// 	array[index].d[face] = ++(*d);
-
-	int opp_face = opposite_face(face);
-	
+	int opp_face = opposite_face(face); // color of bottom face should be same as top face of lower cube
 	adj_cube *head = array[index].link[opp_face];
-	while (head != NULL) {
+	while (head != NULL) { // 
 		int vi = head->index;
 		int vf = head->face;
-		if (array[vi].visit_color[vf] == -1) {
-			array[vi].parent[vf][0] = index;
-			array[vi].parent[vf][1] = face;
-			// array[vi].d[vf] = array[index].d[face]+1; 
-			dfs_visit(array, n, vi, vf);
+		if (array[vi].visit_face[vf] == -1) { 
+			dfs_visit(array, n, vi, vf, mi, mf); // visit, if white
 		}
-		// else if (array[vi].visit_color[vf] == 1) {
-		// 	int wi = array[vi].parent[vf][0];
-		// 	int wf = array[vi].parent[vf][1];
-		// 	if (wi != -1 && wf != -1) {
-		// 		if (array[index].d[face] > array[wi].d[wf]) {
-		// 			array[vi].parent[vf][0] = index;
-		// 			array[vi].parent[vf][1] = face;
-		// 			array[vi].d[vf] = array[index].d[face]+1;
-		// 		}
-		// 	}
-		// }
+		if (array[index].f[face] <= array[vi].f[vf]) {
+			array[index].f[face] = array[vi].f[vf] + 1; // maintain f 
+		}
+		if (array[*mi].f[*mf] < array[index].f[face]) { // maintain the node with maximum f
+			*mi = index;
+			*mf = face;
+		}
 		head = head->ptr;
 	}
-	array[index].visit_color[face] = 1;
-	// find maximum height subtree below it
-	max_subtree(array, n, index, face);
-	// --(*d); // stack is breaking
+	array[index].visit_face[face] = 1; // black
 }
 
-void dfs(cube *array, int n) {
+void dfs(cube *array, int n, int *mi, int *mf) { // dfs gives f for every face of every cube
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < 6; j++) {
-			if (array[i].visit_color[j] == -1) {
-				array[i].d[j] = 1;
-				dfs_visit(array, n, i, j);
+			if (array[i].visit_face[j] == -1) {
+				dfs_visit(array, n, i, j, mi, mf);
 			}
 		}
 	}
 }
 
-void build_graph(cube *array, int n) {
+void build_graph(cube *array, int n) { // a directed edge is made between a face of lighter cube to a same coloured face of a heavier cube
 	for (int i = 0; i < n; i++) {
 		for (int j = i+1; j < n; j++) {
 			for (int k = 0; k < 6; k++) {
-				int color1 = array[j].face_colors[k];
 				for (int l = 0; l < 6; l++) {
-					int color2 = array[i].face_colors[l];
-					if (color1 == color2) {
-						prepend(&(array[i].link[l]), j, k);
+					if (array[j].face_color[k] == array[i].face_color[l]) {
+						insert(&(array[i].link[l]), j, k);
 					}
 				}
 			}
@@ -111,18 +77,8 @@ void build_graph(cube *array, int n) {
 	}
 }
 
-void print(cube *array, int n) {
-	for (int i = 0; i < n; i++) {
-		cout << "cube" << i+1 << " ";
-		for (int j = 0; j < 6; j++) {
-			cout << array[i].d[j] << "," << array[i].f[j] << "\t";
-		}
-		cout << endl;
-	}
-}
-
-void output_helper(cube *array, int n, int ui, int uf) {
-	cout << ui+1;
+void print_output(cube *array, int n, int ui, int uf) {
+	cout << ui+1; // print cube number and face which will be at top
 	if (uf == 0) cout << " front";
 	else if (uf == 1) cout << " back";
 	else if (uf == 2) cout << " left";
@@ -135,55 +91,41 @@ void output_helper(cube *array, int n, int ui, int uf) {
 	while (head != NULL) {
 		int vi = head->index;
 		int vf = head->face;
-		if (array[vi].f[vf] == array[ui].f[uf]-1) {
-			output_helper(array, n, vi, vf);
+		if (array[vi].f[vf] == array[ui].f[uf]-1) { // next cube will same color as opposite face of ui and f one less than upper cube
+			print_output(array, n, vi, vf);
 			break;
 		}
 		head = head->ptr;
 	}
 }
 
-void output(cube *array, int n) {
-	// cout << array[5].parent[4][0] << " " << array[5].parent[4][1] << endl;
-	int ui = 0, uf = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 6; j++) {
-			if (array[i].f[j] > array[ui].f[uf]){
-				ui = i; uf = j;
-			}
-		}
-	}
-	cout << array[ui].f[uf] << endl;
-	output_helper(array, n, ui, uf);
-}
-
 int main() {
 	int n;
 	int c = 1;
-	while (c) {
+	while (1) {
 		cin >> n;
 		if (n == 0) return 0;
 		cube array[n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < 6; j++) {
-				cin >> array[i].face_colors[j];
+				cin >> array[i].face_color[j]; // scan face colors
 			}
 		}
-		// initialise attributes
+		// initialise attributes of adjacency list
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < 6; j++) {
-				array[i].visit_color[j] = -1; // white
-				array[i].d[j] = -1;
-				array[i].parent[j][0] = -1;
-				array[i].parent[j][1] = -1;
+				array[i].visit_face[j] = -1; // white
+				array[i].f[j] = 1; // default 1 (eg for isolated node)
 				array[i].link[j] = NULL;
 			}
 		}
-		build_graph(array, n);
-		dfs(array, n);
-		// print(array, n);
+		build_graph(array, n); // make edges
+		int mi = 0, mf = 0; // mi, mf denote the node with maximum f
+		dfs(array, n, &mi, &mf); 
+		
 		cout << "Case #" << c++ << endl;
-		output(array, n);
+		cout << array[mi].f[mf] << endl; // print maximum height
+		print_output(array, n, mi, mf);  // print cube sequence in tower
 		cout << endl;
 	}	
 }
