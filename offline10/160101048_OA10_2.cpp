f82bfeb56a245961b5b8c7c8292167cb11efffe2
Nitin Kedia <nitinkedia7@gmail.com>
2017-11-07 21:29:00 +0530

    Change multiple outer loop to one outer loop with back tracking, add comments

--- offline10/160101048_OA10_2.cpp
+++ offline10/160101048_OA10_2.cpp
@@ -1,10 +1,10 @@
 // 160101048_OA10_1.cpp: Nitin Kedia
-// Description: Delete all instances of a pattern in a string
+// Description: Delete all instances of a pattern in a string using rolling hash
 
 #include <iostream>
 using namespace std;
 
-int hash_substring(string s) {
+int hash_substring(string s) { // returns hash value of a string
 	int h = (s[0] - 'a') % 23;
 	for (int i = 1; i < s.length(); i++) {
 		h = (h * 26 + (s[i] - 'a')) % 23;
@@ -12,22 +12,22 @@ int hash_substring(string s) {
 	return h;
 }
 
-void hash_string(string s, int *hash, int l) {
-	hash[0] = hash_substring(s.substr(0,l));
+void hash_string(string s, int *hash, int l) { // hash all substrings of length equal to pattern length
+	hash[0] = hash_substring(s.substr(0,l)); // substring of pattern length starting from 0
 
 	int power = 1;
 	for (int i = 0; i < l; i++) {
 		power = (power * 26 ) % 23;
 	}
 
-	for (int i = 1; i < s.length()-l+1; i++) {
-		hash[i] = (hash[i-1] * 26 + (s[i+l-1] - 'a')) % 23;
-		int old_a =  ((s[i-1] - 'a') * power) % 23;
-		hash[i] = (hash[i] - old_a + 23) % 23;
+	for (int i = 1; i < s.length()-l+1; i++) { // next hash values can be calculated on O(1) time using formula
+		hash[i] = (hash[i-1] * 26 + (s[i+l-1] - 'a')) % 23; // add new character
+		int old_a =  ((s[i-1] - 'a') * power) % 23; // old char to be deleted
+		hash[i] = (hash[i] - old_a + 23) % 23; // delete old char
 	}
 }
 
-bool match_string(string s, int hash_s, string p, int hash_p) {
+bool match_string(string s, int hash_s, string p, int hash_p) { // if same hash, check each char and then return yes, otherwise no
 	// cout << s << " " << hash_s << " " << p << " " << hash_p << endl;
 	if (hash_s == hash_p) {
 		if (s == p) return true;
@@ -35,32 +35,30 @@ bool match_string(string s, int hash_s, string p, int hash_p) {
 	return false;
 }
 
-bool delete_pattern(string s, string p) {
+bool delete_pattern(string s, string p) { // delete all instances of the pattern in string
 	string ns = s;
 	int lp = p.length();
-	int hash_p = hash_substring(p);
+	int hash_p = hash_substring(p); // hash value of p
 
-	do {
-		s = ns;
-		int ls = s.length();
+	int ls = ns.length();
+	int hash[ls];
+	hash_string(ns, hash, lp);
 
-		int hash[ls];
-		hash_string(s, hash, lp);
+	for (int j = 0; j < ns.length(); j++) { // delete pattern from string
+		if (j < ns.length()-lp+1) { // atleast lp characters must remain
+			if (match_string(ns.substr(j,lp), hash[j], p, hash_p)) {
+				ns.erase(j, lp);
 
-		ns = "";
-		for (int j = 0; j < ls; j++) {
-			if (j < ls-lp+1) {
-				if (match_string(s.substr(j,lp), hash[j], p, hash_p)) {
-					j = j + lp - 1; 
-				}
-				else {
-					ns = ns + s[j];
+				if (ns.length() < lp) break;
+				hash_string(ns, hash, lp); // rehash the deleted string
+
+				for (int k = 0; k <= lp; k++) { // decrement j till j-lp if possible, to check newly generated instances in next iteration
+					if (j-1 >= -1) j--;
+					else break;
 				}
 			}
-			else ns = ns + s[j];
 		}
-		// cout << s << " " << p << " " << ns << endl;
-	} while (ns != s && ns.length() >= lp);
+	}
 	cout << ns << endl;
 }
 
@@ -68,7 +66,7 @@ int main() {
 	int t;
 	cin >> t;
 
-	for (int i = 0; i < t; i++) {
+	for (int i = 0; i < t; i++) { // for each instance execute delete pattern
 		string s, p;
 		cin >> s >> p;
 		// cout << s << " " << p;
