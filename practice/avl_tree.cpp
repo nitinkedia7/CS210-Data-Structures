5baf0e01d8a141224bfa6d434ce1b7a22291f3a2
Nitin Kedia <nitinkedia7@gmail.com>
2017-11-12 22:42:46 +0530

    Add AVL tree delete

--- practice/avl_tree.cpp
+++ practice/avl_tree.cpp
@@ -63,9 +63,32 @@ node *insert(node *head, int key) {
 
 	head->height = 1 + max(height(head->left), height(head->right));
 
-	int diff = height(head->left) - height(head->right);
+	// int diff = height(head->left) - height(head->right);
+	// if (diff > 1) {
+	// 	if (key <= head->left->key) {			
+	// 		return right_rotate(head);
+	// 	}
+	// 	else {
+	// 		// double rotate
+	// 		head->left = left_rotate(head->left);
+	// 		return right_rotate(head);
+	// 	}
+	// }
+	// else if (diff < -1) {
+	// 	if (key > head->right->key) {
+	// 		return left_rotate(head);
+	// 	}
+	// 	else {
+	// 		// doudle rotate
+	// 		head->right = right_rotate(head->right);
+	// 		return left_rotate(head);
+	// 	}
+	// }
+	// return head;
+		int diff = height(head->left) - height(head->right);
 	if (diff > 1) {
-		if (key <= head->left->key) {			
+		int diff2 = height(head->left->left) - height(head->left->right);
+		if (diff2 >= 0) {			
 			return right_rotate(head);
 		}
 		else {
@@ -75,7 +98,71 @@ node *insert(node *head, int key) {
 		}
 	}
 	else if (diff < -1) {
-		if (key > head->right->key) {
+		int diff3 = height(head->right->left) - height(head->right->right);
+		if (diff3 <= 0) {
+			return left_rotate(head);
+		}
+		else {
+			// doudle rotate
+			head->right = right_rotate(head->right);
+			return left_rotate(head);
+		}
+	}
+	return head;
+}
+
+node *find_min(node *head) {
+	if (head == NULL || head->left == NULL) return head;
+	return head->left; 
+}
+
+node *delete_node(node *head, int key) {
+	if (head == NULL) return NULL;
+
+	if (key < head->key) {
+		head->left = delete_node(head->left, key);
+	}
+	else if (key > head->key) {
+		head->right = delete_node(head->right, key);
+	}
+	else {
+		node *temp;
+		if (head->left == NULL || head->right == NULL) {
+			if (head->left == NULL) temp = head->right;
+			else temp = head->left;
+			if (temp == NULL) {
+				temp = head;
+				head = NULL;
+			}
+			else *head = *temp;
+			delete temp;
+
+		}
+		else {
+			temp = find_min(head->right);
+			head->key = temp->key;
+			head->right = delete_node(head->right, temp->key);
+		}
+	}
+	if (head == NULL) return NULL;
+
+	head->height = 1 + max(height(head->left), height(head->right));
+
+	int diff = height(head->left) - height(head->right);
+	if (diff > 1) {
+		int diff2 = height(head->left->left) - height(head->left->right);
+		if (diff2 >= 0) {			
+			return right_rotate(head);
+		}
+		else {
+			// double rotate
+			head->left = left_rotate(head->left);
+			return right_rotate(head);
+		}
+	}
+	else if (diff < -1) {
+		int diff3 = height(head->right->left) - height(head->right->right);
+		if (diff3 <= 0) {
 			return left_rotate(head);
 		}
 		else {
@@ -98,22 +185,35 @@ void preorder(node *head) {
 
 int main() {
 	node *root = NULL;
-	root = insert(root, 10);
+	root = insert(root, 9);
 	preorder(root);
 	cout << endl;
-	root = insert(root, 20);
+	root = insert(root, 5);
 	preorder(root);
 		cout << endl;
-	root = insert(root, 30);
+	root = insert(root, 10);
 	preorder(root);
 		cout << endl;
-	root = insert(root, 40);
+	root = insert(root, 0);
 	preorder(root);
 		cout << endl;
-	root = insert(root, 50);
+	root = insert(root, 6);
 	preorder(root);
 		cout << endl;
-	root = insert(root, 25);
+	root = insert(root, 11);
 	preorder(root);
 		cout << endl;
+		root = insert(root, -1);
+	preorder(root);
+		cout << endl;
+		root = insert(root, 1);
+	preorder(root);
+		cout << endl;
+			root = insert(root, 2);
+	preorder(root);
+		cout << endl;
+
+	root = delete_node(root, 10);
+	preorder(root);
+	cout << endl;
 }
