ebfe446212238b88e680ccaceff153098e869405
Nitin Kedia <nitinkedia7@gmail.com>
2017-11-12 16:25:04 +0530

    Improve complexity, using one tree to find freq and then sort by freq

--- offline11/160101048_OA11_2.cpp
+++ offline11/160101048_OA11_2.cpp
@@ -1,15 +1,15 @@
 // 160101048_OA11_2.cpp: Nitin Kedia
-// Description: Arrange sequence based on number of occurences using two BST sorts
+// Description: Arrange sequence based on number of occurences first by finding frequency and then sort by frequency
 
 #include <iostream>
 using namespace std;
 
-struct node {
+struct node { // struct for each distinct key and its freq
 	int key, freq;
 	node *left, *right;
 };
 
-node *create_node(int key, int freq) {
+node *create_node(int key, int freq) { // initialise a node with key and freq
 	node *temp = new node;
 	temp->key = key;
 	temp->freq = freq;
@@ -17,85 +17,70 @@ node *create_node(int key, int freq) {
 	temp->right = NULL;
 	return temp;
 }
-void insert(node **pHead, int key, int freq) {
-	node *y = NULL;
-	node *x = *pHead; 
-	while (x != NULL) {
-		y = x;
-		if (x->freq < freq) {
-			x = x->right;
-		}
-		else if (x->freq == freq && x->key > key) {
-			x = x->right;
-		}
-		else {
-			x = x->left;
-		}
-	}
-	node *temp = create_node(key, freq);
-
-	if (y == NULL) {
-		*pHead = temp;
-		return;
-	}
-	if (y->freq < freq) {
-		y->right = temp;
-	}
-	else if (y->freq == freq && y->key > key) {
-		y->right = temp;
-	}
-	else y->left = temp;
+// first, we find frequency by inserting all keys into a BST, incrementing freq if match is found
+void insert_by_key(node** pHead, int key, int freq) {
+    node *head = *pHead;
+    if (head == NULL) {
+    	*pHead = create_node(key, freq); // make root
+    	return;
+    }
+   	if (head->key == key) { // incoming key already present in tree, so increment freq
+   		head->freq += freq;
+   	}
+    else if (key < head->key) { // else insert as a leaf node
+        insert_by_key(&(head->left), key, freq);
+    }
+	else insert_by_key(&(head->right), key, freq);   
+}
+// Afterward by make BST2 according to frequency of each distict key found from BST1
+void insert_by_freq(node** pHead, int key, int freq) {
+    node *head = *pHead;
+    if (head == NULL) {
+    	*pHead = create_node(key, freq); // make root
+    	return;
+    }
+    if (freq < head->freq) { // incoming freq is less
+        insert_by_freq(&(head->left), key, freq);
+    }
+    else if (freq == head->freq && key > head->key) { // incoming freq is same but key is bigger
+        insert_by_freq(&(head->left), key, freq);
+    }
+	else insert_by_freq(&(head->right), key, freq);
 }
 
-void store_inorder(node *head, int *array, int *i) {
+void inorder(node *head1, node **pHead2) { // inserting elements from BST1 to BST2 in inorder seq
+	if (head1 == NULL) return;
+	else {
+		inorder(head1->left, pHead2);
+		insert_by_freq(pHead2, head1->key, head1->freq);
+		inorder(head1->right, pHead2);
+	}
+}
+
+void print_rev_inorder(node *head) { // print BST2 in reverse inorder to sort in decreasing order of occurence
 	if (head == NULL) return;
 	else {
-		store_inorder(head->left, array, i);
-		for (int j = 0; j < head->freq; j++) {
-			array[*i] = head->key;
-			(*i)++;
-		} 
-		store_inorder(head->right, array, i);
-	}
-}
-
-void bst_sort(int *array, int *sorted_array, int n) {
-	node *head = NULL;
-	int key = array[0];
-	int freq = 1;
-	for (int i = 1; i < n; i++) {
-		if (array[i] == key) {
-			freq++;
+		print_rev_inorder(head->right);
+		for (int i = 0; i < head->freq; i++) { // print each key freq times
+			cout << head->key << " ";
 		}
-		else {
-			insert(&head, key, freq);
-			key = array[i];
-			freq = 1;
-		}
+		print_rev_inorder(head->left);
 	}
-	insert(&head, key, freq);
-
-	int i = 0;
-	store_inorder(head, sorted_array, &i);
 }
 
 int main() {
 	int n;
 	cin >> n;
 
-	int in_seq[n];
-	for (int i = 0; i < n; i++) {
-		cin >> in_seq[i];	
+	int key;
+	node *head1 = NULL;
+	for (int i = 0; i < n; i++) { // build BST1
+		cin >> key;
+		insert_by_key(&head1, key, 1);	
 	}
 
-	int inter_seq[n];
-	bst_sort(in_seq, inter_seq, n);
-	int out_seq[n];
-	bst_sort(inter_seq, out_seq, n);
-
-	for (int i = n-1; i >= 0; i--) {
-		cout << out_seq[i] << " "; 
-	}
+	node *head2 = NULL;
+	inorder(head1, &head2); // build BST2
+	print_rev_inorder(head2); // print BST2
 	cout << endl;
-	return 0;
 }
